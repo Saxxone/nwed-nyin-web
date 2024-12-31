aa5755add42f734eade4e0f0a7d26372f82d8317
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-12-31 17:50:57 +0000

    minor updates

--- components/dictionary/Definition.vue
+++ components/dictionary/Definition.vue
@@ -11,12 +11,12 @@ const props = defineProps<Props>();
 
 <template>
   <section class="border rounded-md card text-sm word-wrap mb-4 select-text break-words ove">
-    <h5 class="scroll-m-20 text-2xl items-end capitalize font-bold tracking-tight lg:text-xl mb-2">
+    <h5 class="scroll-m-20 text-2xl items-end capitalize font-bold tracking-tight lg:text-xl mb-1">
       <div>{{ props.word.term }}</div>
 
       <div class="text-gray-500 text-sm font-medium block" v-if="props.word.alt_spelling">({{ props.word.alt_spelling }})</div>
     </h5>
-    <p class="mb-2 font-serif text-xs">
+    <p class="mb-2 font-serif text-xs" v-if="props.word.pronunciation">
       {{ `${props.word.pronunciation?.startsWith("/") ? "" : "/"}${props.word.pronunciation}${props.word.pronunciation?.endsWith("/") ? "" : "/"}` }}
     </p>
 
@@ -25,14 +25,14 @@ const props = defineProps<Props>();
         <div v-if="props.more || index === 0">
           <p class="text-xs italic text-muted">{{ definition.part_of_speech.name }}</p>
           <p>{{ definition.meaning }}</p>
-          <div class="my-1">
+          <div class="my-1" v-if="definition.examples.length > 0">
             <h6 class="text-xs mt-2 text-muted">Examples:</h6>
 
             <div>
               <p v-for="example in definition.examples" class="text-xs">{{ example.sentence }}</p>
             </div>
 
-            <div>
+            <div v-if="definition.synonyms.length > 0">
               <h6 class="text-xs mt-2 text-muted">Synonyms:</h6>
               <div>
                 <span v-for="synonym in definition.synonyms" class="text-xs">{{ synonym.synonym }}, </span>
