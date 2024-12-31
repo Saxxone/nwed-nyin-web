7aaaf8424144ddc6dc56c6bf68a1d3c9112cce00
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-12-31 18:43:22 +0000

    fix

--- components/dictionary/Definition.vue
+++ components/dictionary/Definition.vue
@@ -11,7 +11,7 @@ const props = defineProps<Props>();
 
 <template>
   <NuxtLink
-    :to="`${routes.dictionary.view(props.word.alt_spelling ? props.word.alt_spelling : props.word.term)}`"
+    :to="`${routes.dictionary.view(props.word.alt_spelling?.length ? props.word.alt_spelling : props.word.term)}`"
     class="border block rounded-md card text-sm word-wrap mb-4 select-text break-words ove">
     <h5 class="scroll-m-20 text-2xl items-end capitalize font-bold tracking-tight lg:text-xl mb-1">
       <div>{{ props.word.term }}</div>
@@ -49,7 +49,7 @@ const props = defineProps<Props>();
       </div>
 
       <NuxtLink
-        :to="`${routes.dictionary.view(props.word.alt_spelling ? props.word.alt_spelling : props.word.term)}`"
+        :to="`${routes.dictionary.view(props.word.alt_spelling?.length ? props.word.alt_spelling : props.word.term)}`"
         class="text-blue-500 text-xs"
         v-if="props.word.definitions.length > 0 && !props.more && $props.word.term && $props.word.related_to"
         >see more</NuxtLink
