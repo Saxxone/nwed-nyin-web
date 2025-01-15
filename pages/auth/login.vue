6843cc8983506d8a2b7f4460fdd8297ddbb3b995
Udoekpo Stephen <saxxone17@gmail.com>
2025-01-15 16:38:57 +0000

    minor fix

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -86,7 +86,7 @@ onMounted(async () => {
             <div class="g_id_signin" />
           </div>
           <p class="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our
+            By signing up, you agree to our
             <a href="/terms" class="underline underline-offset-4 hover:text-primary"> Terms of Service </a>
             and
             <a href="/privacy" class="underline underline-offset-4 hover:text-primary"> Privacy Policy </a>
