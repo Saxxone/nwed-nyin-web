48794535e1b03491e14543b4af0a1592045e1301
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2025-01-27 22:18:21 +0000

    attempt prfile url loading

--- components/app/TopBar.vue
+++ components/app/TopBar.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
-import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+import { Menubar, MenubarMenu } from "@/components/ui/menubar";
 import routes from "@/utils/routes";
+import { useAuthStore } from "~/store/auth";
+
+let authStore;
 
 const pages = [
   {
@@ -12,20 +15,25 @@ const pages = [
     route: routes.dictionary.list,
   },
 ];
+
+onMounted(() => {
+  authStore = useAuthStore();
+});
 </script>
 
 <template>
   <div>
-    <AppDarkMode />
+    <div>
+      <AppDarkMode />
+      <ClientOnly fallbackTag="span">
+        <div v-if="authStore.user?.img">
+          <img :src="'api.nwednyin.org/public/'+authStore.user.img" alt="" />
+        </div>
+      </ClientOnly>
+    </div>
     <Menubar>
       <MenubarMenu class="flex justify-between">
-        <NuxtLink
-          :to="page.route"
-          v-for="page in pages"
-          :key="page.name"
-          class="px-2"
-          >{{ page.name }}</NuxtLink
-        >
+        <NuxtLink :to="page.route" v-for="page in pages" :key="page.name" class="px-2">{{ page.name }}</NuxtLink>
       </MenubarMenu>
     </Menubar>
   </div>
